e72a4b958189838eaf26f5a92ff1f7925fe1646d
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-04-11 13:21:51 -0300

    Create e x e r c i c i o 14.php

---
+++ e x e r c i c i o 14.php
@@ -0,0 +1,12 @@
+<?php
+echo"
+********************
+e x e r c i c i o 14
+********************\n"
+echo"Aqui está uma lista dos numeros impares de 1 a 50.\n"
+$numero = 0;
+while($numero < 51){
+    if ($numero%2 == 1 )
+    {print "$numero\n";}
+    $numero ++;
+}
