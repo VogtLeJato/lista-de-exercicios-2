fc7cb7d93d49dc30d1d7bbcbad13f86723b424e5
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-04-11 13:34:47 -0300

    Create exercicio2_16.php

---
+++ exercicio2_16.php
@@ -0,0 +1,44 @@
+<?php
+echo"
+********************
+e x e r c i c i o 16
+********************\n";
+echo"digite cinco numeros e eu farei uma media deles\n";
+$numero_um = fgets(STDIN);
+$numero_dois = fgets(STDIN);
+$numero_tres = fgets(STDIN);
+$numero_quatro = fgets(STDIN);
+$numero_cinco = fgets(STDIN);
+
+sleep(2);
+
+echo"A cho"
+
+sleep(3);
+
+echo"Acho, que estou com !@$$! p$o¨&mas";
+
+Sleep(4);
+
+echo"*Error468*";
+
+sleep(5);
+
+echo "Brincadeira";
+
+
+
+if ($numero_um > $numero_dois and $numero_um > $numero_tres and $numero_um > $numero_quatro and $numero_um > $numero_cinco)
+ {echo"o $numero_um é o maior dos cinco numeros";}
+
+if ($numero_dois > $numero_um and $numero_dois > $numero_tres and $numero_dois > $numero_quatro and $numero_dois > $numero_cinco)
+ {echo"o $numero_dois é o maior dos cinco numeros";}
+
+if ($numero_tres > $numero_um and $numero_tres > $numero_dois and $numero_tres > $numero_quatro and $numero_tres > $numero_cinco)
+ {echo"o $numero_tres é o maior dos cinco numeros";}
+
+if ($numero_quatro > $numero_um and $numero_quatro > $numero_dois and $numero_quatro > $numero_tres and $numero_quatro > $numero_cinco)
+ {echo"o $numero_quatro é o maior dos cinco numeros";}
+
+if ($numero_cinco > $numero_um and $numero_cinco > $numero_dois and $numero_cinco > $numero_tres and $numero_cinco > $numero_quatro)
+ {echo"o $numero_cinco é o maior dos cinco numeros";}
