e1f49d4b01f5c406e372e395bfb01a47b4c5ff57
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-04-11 13:15:52 -0300

    Create exercicio2_12.php

---
+++ exercicio2_12.php
@@ -0,0 +1,10 @@
+<?php
+echo"
+********************
+e x e r c i c i o 12
+********************\n"
+echo"Aqui está uma lista dos numeros de 1 a 100\n"
+$numero = 1;
+while($numero < 101){
+    echo "$numero\n";
+    $numero ++;}
