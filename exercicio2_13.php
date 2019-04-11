b420b1ea8e1b6fa51bf1440cf1a3f1b56680fd07
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-04-11 13:18:50 -0300

    Create exercicio2_13.php

---
+++ exercicio2_13.php
@@ -0,0 +1,11 @@
+<?php
+echo"
+********************
+e x e r c i c i o 13
+********************\n"
+echo"E aqui uma de 100 a 1.\n"
+$numero = 100;
+while($numero > 0){
+    print"$numero\n";
+    $numero --;
+}
