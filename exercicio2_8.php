48b97c9aa8d8286ada03a007570429ce8346b583
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-04-11 12:57:58 -0300

    Create exercicio2_8.php

---
+++ exercicio2_8.php
@@ -0,0 +1,18 @@
+<?php
+echo"
+*******************
+e x e r c i c i o 8
+*******************"
+echo"informe o preço do produto 1\n";
+ $preço_produto_um = fgets(STDIN);
+ $preço_produto_dois = fgets(STDIN);
+ $preço_produto_tres = fgets(STDIN);
+
+ if ($preço_produto_um < $preço_produto_dois and $preço_produto_um < $preço_produto_tres)
+     {echo"compre o primeiro produto de preço R$$preço_produto_um";}
+ 
+ if ($preço_produto_dois < $preço_produto_um and $preço_produto_dois < $preço_produto_tres)
+     {echo"compre o segundo produto de preço R$$preço_produto_dois";}
+ 
+ if ($preço_produto_tres < $preço_produto_dois and $preço_produto_tres < $preço_produto_um)
+     {echo"compre o terceiro produto de preço R$$preço_produto_tres";}
