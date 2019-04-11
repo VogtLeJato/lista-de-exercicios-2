22bf11b3845f11153a8a59ae481f2f158755f42e
*VogtLe 威廉* <48601875+VogtLeJato@users.noreply.github.com>
2019-04-11 12:47:41 -0300

    Create exercicio2_5.php

---
+++ exercicio2_5.php
@@ -0,0 +1,17 @@
+<?php
+print"digite a primeira nota do aluno";
+  $nota_parcial_1 = fgets(STDIN);
+
+print"digite a segunda nota do aluno";
+  $nota_parcial_2 = fgets(STDIN);
+
+     $nota_final = ($nota_parcial_1 + $nota_parcial_2)/2;
+     $nota_final = (float)($nota_final);
+     
+     if ($nota_final==10)
+     {print"Situação do aluno: Aprovado com distinção";}
+            
+            elseif($nota_final==7 or $nota_final > 7)
+            {print"Situação do aluno: Aprovado";}
+
+                 else {print "Situação do aluno: Não aprovado";}
